7da3d8aef63fd847b1386cb668a6a6ae26e3a3b1
Евгений Минин <evgenminin1988@mail.ru>
2023-06-27 11:25:23 +0300

    initial

---
+++ src/working_with_database.py
@@ -0,0 +1,17 @@
+class DBManager:
+    pass
+
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    def get_all_vacancies_count(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self):
+        pass
